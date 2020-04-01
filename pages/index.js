c3252a5b9017188f3b5c482b348379183bf64825
Isaac Graves <igraves@hbs.edu>
2020-04-01 14:13:57 -0400

    added api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,40 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Index = styled.div`
+  line-height: 2em;
+`;
+
+const Button = styled.span`
+  font-weight: bold;
+  background-color: black;
+  color: white;
+  padding: 0 10px;
+  border-radius: 5px;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+
 export default () => {
-  return <div>This content is templated by next.js</div>
+  const [msg, setMsg] = useState(false);
+
+  const fetchMsg = async () => {
+    let response = await fetch('/api/hello');
+    response = await response.json();
+    response = JSON.stringify(response, null, 2);
+    setMsg(response);
+  }
+
+  return (
+    <Index>
+      <div>This content is templated by next.js</div>
+      <div>
+        <Button onClick={fetchMsg}>Fetch</Button> a message from the next.js api
+        {msg && <div>/api/hello says: {msg}</div>}
+      </div>
+    </Index>
+  )
 }
